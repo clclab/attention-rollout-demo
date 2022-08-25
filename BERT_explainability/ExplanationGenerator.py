2e1a3f8ec28b5d61a86527d16a9050e752eca084
Martijn van Beers <mail_dev@martijn.at>
2022-08-25 14:50:53 +0200

    try to make it work quick and dirty

---
+++ BERT_explainability/ExplanationGenerator.py
@@ -0,0 +1,165 @@
+import argparse
+import numpy as np
+import torch
+import glob
+
+from captum._utils.common import _get_module_from_name
+
+# compute rollout between attention layers
+def compute_rollout_attention(all_layer_matrices, start_layer=0):
+    # adding residual consideration- code adapted from https://github.com/samiraabnar/attention_flow
+    num_tokens = all_layer_matrices[0].shape[1]
+    batch_size = all_layer_matrices[0].shape[0]
+    eye = torch.eye(num_tokens).expand(batch_size, num_tokens, num_tokens).to(all_layer_matrices[0].device)
+    all_layer_matrices = [all_layer_matrices[i] + eye for i in range(len(all_layer_matrices))]
+    matrices_aug = [all_layer_matrices[i] / all_layer_matrices[i].sum(dim=-1, keepdim=True)
+                          for i in range(len(all_layer_matrices))]
+    joint_attention = matrices_aug[start_layer]
+    for i in range(start_layer+1, len(matrices_aug)):
+        joint_attention = matrices_aug[i].bmm(joint_attention)
+    return joint_attention
+
+class Generator:
+    def __init__(self, model, key="bert.encoder.layer"):
+        self.model = model
+        self.key = key
+        self.model.eval()
+
+    def forward(self, input_ids, attention_mask):
+        return self.model(input_ids, attention_mask)
+
+    def _calculate_gradients(self, output, index, do_relprop=True):
+        if index == None:
+            index = np.argmax(output.cpu().data.numpy(), axis=-1)
+
+        one_hot_vector = (torch.nn.functional
+                .one_hot(
+                        # one_hot requires ints
+                        torch.tensor(index, dtype=torch.int64),
+                        num_classes=output.size(-1)
+                    )
+                # but requires_grad_ needs floats
+                .to(torch.float)
+            ).to(output.device)
+
+        hot_output = torch.sum(one_hot_vector.clone().requires_grad_(True) * output)
+        self.model.zero_grad()
+        hot_output.backward(retain_graph=True)
+
+        if do_relprop:
+            return self.model.relprop(one_hot_vector, alpha=1)
+
+    def generate_LRP(self, input_ids, attention_mask,
+                     index=None, start_layer=11):
+        output = self.model(input_ids=input_ids, attention_mask=attention_mask)[0]
+
+        if index == None:
+            index = np.argmax(output.cpu().data.numpy(), axis=-1)
+
+        self._calculate_gradients(output, index)
+
+        cams = []
+        blocks = _get_module_from_name(self.model, self.key)
+        for blk in blocks:
+            grad = blk.attention.self.get_attn_gradients()
+            cam = blk.attention.self.get_attn_cam()
+            cam = cam[0].reshape(-1, cam.shape[-1], cam.shape[-1])
+            grad = grad[0].reshape(-1, grad.shape[-1], grad.shape[-1])
+            cam = grad * cam
+            cam = cam.clamp(min=0).mean(dim=0)
+            cams.append(cam.unsqueeze(0))
+        rollout = compute_rollout_attention(cams, start_layer=start_layer)
+        rollout[:, 0, 0] = rollout[:, 0].min()
+        return rollout[:, 0]
+
+
+    def generate_LRP_last_layer(self, input_ids, attention_mask,
+                     index=None):
+        output = self.model(input_ids=input_ids, attention_mask=attention_mask)[0]
+        if index == None:
+            index = np.argmax(output.cpu().data.numpy(), axis=-1)
+
+        self._calculate_gradients(output, index)
+
+        cam = _get_module_from_name(self.model, self.key)[-1].attention.self.get_attn_cam()[0]
+        cam = cam.clamp(min=0).mean(dim=0).unsqueeze(0)
+        cam[:, 0, 0] = 0
+        return cam[:, 0]
+
+    def generate_full_lrp(self, input_ids, attention_mask,
+                     index=None):
+        output = self.model(input_ids=input_ids, attention_mask=attention_mask)[0]
+
+        if index == None:
+            index = np.argmax(output.cpu().data.numpy(), axis=-1)
+
+        cam = self._calculate_gradients(output, index)
+        cam = cam.sum(dim=2)
+        cam[:, 0] = 0
+        return cam
+
+    def generate_attn_last_layer(self, input_ids, attention_mask,
+                     index=None):
+        output = self.model(input_ids=input_ids, attention_mask=attention_mask)[0]
+        cam = _get_module_from_name(self.model, self.key)[-1].attention.self.get_attn()[0]
+        cam = cam.mean(dim=0).unsqueeze(0)
+        cam[:, 0, 0] = 0
+        return cam[:, 0]
+
+    def generate_rollout(self, input_ids, attention_mask, start_layer=0, index=None):
+        self.model.zero_grad()
+        output = self.model(input_ids=input_ids, attention_mask=attention_mask)[0]
+        blocks = _get_module_from_name(self.model, self.key)
+        all_layer_attentions = []
+        for blk in blocks:
+            attn_heads = blk.attention.self.get_attn()
+            avg_heads = (attn_heads.sum(dim=1) / attn_heads.shape[1]).detach()
+            all_layer_attentions.append(avg_heads)
+        rollout = compute_rollout_attention(all_layer_attentions, start_layer=start_layer)
+        rollout[:, 0, 0] = 0
+        return rollout[:, 0]
+
+    def generate_attn_gradcam(self, input_ids, attention_mask, index=None):
+        output = self.model(input_ids=input_ids, attention_mask=attention_mask)[0]
+
+        if index == None:
+            index = np.argmax(output.cpu().data.numpy(), axis=-1)
+
+        self._calculate_gradients(output, index)
+
+        cam = _get_module_from_name(self.model, self.key)[-1].attention.self.get_attn()
+        grad = _get_module_from_name(self.model, self.key)[-1].attention.self.get_attn_gradients()
+
+        cam = cam[0].reshape(-1, cam.shape[-1], cam.shape[-1])
+        grad = grad[0].reshape(-1, grad.shape[-1], grad.shape[-1])
+        grad = grad.mean(dim=[1, 2], keepdim=True)
+        cam = (cam * grad).mean(0).clamp(min=0).unsqueeze(0)
+        cam = (cam - cam.min()) / (cam.max() - cam.min())
+        cam[:, 0, 0] = 0
+        return cam[:, 0]
+
+    def generate_rollout_attn_gradcam(self, input_ids, attention_mask, index=None, start_layer=0):
+        # rule 5 from paper
+        def avg_heads(cam, grad):
+            return (grad * cam).clamp(min=0).mean(dim=-3)
+
+        # rule 6 from paper
+        def apply_self_attention_rules(R_ss, cam_ss):
+            return torch.matmul(cam_ss, R_ss)
+
+        output = self.model(input_ids=input_ids, attention_mask=attention_mask)[0]
+        blocks = _get_module_from_name(self.model, self.key)
+
+        num_tokens = input_ids.size(-1)
+        R = torch.eye(num_tokens).expand(output.size(0), -1, -1).clone().to(output.device)
+
+        for i, blk in enumerate(model.roberta.encoder.layer):
+            if i < start_layer:
+                continue
+            grad = blk.attention.self.get_attn_gradients().detach()
+            cam = blk.attention.self.get_attn().detach()
+            cam = avg_heads(cam, grad)
+            joint = apply_self_attention_rules(R, cam)
+            R += joint
+        return R[:, 0, 1:-1]
+
--- app.py
+++ app.py
@@ -1,7 +1,194 @@
+import sys
 import gradio
 
-def greet(name):
-    return f"Hello {name}. Check back soon for real content"
+sys.path.append("BERT_explainability")
 
-iface = gradio.Interface(fn=greet, inputs="text", outputs="text")
+import torch
+
+from BERT_explainability.ExplanationGenerator import Generator
+from BERT_explainability.roberta2 import RobertaForSequenceClassification
+from transformers import AutoTokenizer
+
+from captum.attr import (
+        visualization
+    )
+import torch
+
+# from https://discuss.pytorch.org/t/using-scikit-learns-scalers-for-torchvision/53455
+class PyTMinMaxScalerVectorized(object):
+    """
+    Transforms each channel to the range [0, 1].
+    """
+    def __init__(self, dimension=-1):
+        self.d = dimension
+    def __call__(self, tensor):
+        d = self.d
+        scale = 1.0 / (tensor.max(dim=d, keepdim=True)[0] - tensor.min(dim=d, keepdim=True)[0])
+        tensor.mul_(scale).sub_(tensor.min(dim=d, keepdim=True)[0])
+        return tensor
+
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+model = RobertaForSequenceClassification.from_pretrained("textattack/roberta-base-SST-2").to(device)
+model.eval()
+tokenizer = AutoTokenizer.from_pretrained("textattack/roberta-base-SST-2")
+# initialize the explanations generator
+explanations = Generator(model, "roberta")
+
+classifications = ["NEGATIVE", "POSITIVE"]
+
+# rule 5 from paper
+def avg_heads(cam, grad):
+    cam = (
+            (grad * cam)
+            .clamp(min=0)
+            .mean(dim=-3)
+        )
+    # set negative values to 0, then average
+#    cam = cam.clamp(min=0).mean(dim=0)
+    return cam
+
+# rule 6 from paper
+def apply_self_attention_rules(R_ss, cam_ss):
+    R_ss_addition = torch.matmul(cam_ss, R_ss)
+    return R_ss_addition
+
+def generate_relevance(model, input_ids, attention_mask, index=None, start_layer=0):
+    output = model(input_ids=input_ids, attention_mask=attention_mask)[0]
+    if index == None:
+        #index = np.expand_dims(np.arange(input_ids.shape[1])
+        # by default explain the class with the highest score
+        index = output.argmax(axis=-1).detach().cpu().numpy()
+
+    # create a one-hot vector selecting class we want explanations for
+    one_hot = (torch.nn.functional
+            .one_hot(torch.tensor(index, dtype=torch.int64), num_classes=output.size(-1))
+            .to(torch.float)
+            .requires_grad_(True)
+        ).to(device)
+    print("ONE_HOT", one_hot.size(), one_hot)
+    one_hot = torch.sum(one_hot * output)
+    model.zero_grad()
+    # create the gradients for the class we're interested in
+    one_hot.backward(retain_graph=True)
+
+    num_tokens = model.roberta.encoder.layer[0].attention.self.get_attn().shape[-1]
+    print(input_ids.size(-1), num_tokens)
+    R = torch.eye(num_tokens).expand(output.size(0), -1, -1).clone().to(device)
+
+    for i, blk in enumerate(model.roberta.encoder.layer):
+        if i < start_layer:
+            continue
+        grad = blk.attention.self.get_attn_gradients()
+        cam = blk.attention.self.get_attn()
+        cam = avg_heads(cam, grad)
+        joint = apply_self_attention_rules(R, cam)
+        R += joint
+    return output, R[:, 0, 1:-1]
+
+def visualize_text(datarecords, legend=True):
+    dom = ["<table width: 100%>"]
+    rows = [
+        "<tr><th>True Label</th>"
+        "<th>Predicted Label</th>"
+        "<th>Attribution Label</th>"
+        "<th>Attribution Score</th>"
+        "<th>Word Importance</th>"
+    ]
+    for datarecord in datarecords:
+        rows.append(
+            "".join(
+                [
+                    "<tr>",
+                    format_classname(datarecord.true_class),
+                    format_classname(
+                        "{0} ({1:.2f})".format(
+                            datarecord.pred_class, datarecord.pred_prob
+                        )
+                    ),
+                    format_classname(datarecord.attr_class),
+                    format_classname("{0:.2f}".format(datarecord.attr_score)),
+                    format_word_importances(
+                        datarecord.raw_input_ids, datarecord.word_attributions
+                    ),
+                    "<tr>",
+                ]
+            )
+        )
+
+    if legend:
+        dom.append(
+            '<div style="border-top: 1px solid; margin-top: 5px; \
+            padding-top: 5px; display: inline-block">'
+        )
+        dom.append("<b>Legend: </b>")
+
+        for value, label in zip([-1, 0, 1], ["Negative", "Neutral", "Positive"]):
+            dom.append(
+                '<span style="display: inline-block; width: 10px; height: 10px; \
+                border: 1px solid; background-color: \
+                {value}"></span> {label}  '.format(
+                    value=_get_color(value), label=label
+                )
+            )
+        dom.append("</div>")
+
+    dom.append("".join(rows))
+    dom.append("</table>")
+    html = "".join(dom)
+
+    return html
+
+def show_explanation(model, input_ids, attention_mask, index=None, start_layer=0):
+    # generate an explanation for the input
+    output, expl = generate_relevance(model, input_ids, attention_mask, index=index, start_layer=start_layer)
+    print(output.shape, expl.shape)
+    # normalize scores
+    scaler = PyTMinMaxScalerVectorized()
+
+    norm = scaler(expl)
+    # get the model classification
+    output = torch.nn.functional.softmax(output, dim=-1)
+
+
+    vis_data_records = []
+    for record in range(input_ids.size(0)):
+        classification = output[record].argmax(dim=-1).item()
+        class_name = classifications[classification]
+        nrm = norm[record]
+
+        # if the classification is negative, higher explanation scores are more negative
+        # flip for visualization
+        if class_name == "NEGATIVE":
+            nrm *= (-1)
+        tokens = tokenizer.convert_ids_to_tokens(input_ids[record].flatten())[1:0 - ((attention_mask[record] == 0).sum().item() + 1)]
+        print([(tokens[i], nrm[i].item()) for i in range(len(tokens))])
+        vis_data_records.append(visualization.VisualizationDataRecord(
+                                    nrm,
+                                    output[record][classification],
+                                    classification,
+                                    classification,
+                                    index,
+                                    1,
+                                    tokens,
+                                    1))
+    return visualize_text(vis_data_records)
+
+def run(input_text):
+    text_batch = [input_text]
+    encoding = tokenizer(text_batch, return_tensors='pt')
+    input_ids = encoding['input_ids'].to(device)
+    attention_mask = encoding['attention_mask'].to(device)
+
+    # true class is positive - 1
+    true_class = 1
+
+    html = show_explanation(model, input_ids, attention_mask)
+    return html
+
+iface = gradio.Interface(fn=greet, inputs="text", outputs="html", examples=[["This movie was the best movie I have ever seen! some scenes were ridiculous, but acting was great"], ["I really didn't like this movie. Some of the actors were good, but overall the movie was boring"]])
 iface.launch()
