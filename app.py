cd3f11060d844ce7fd5306719f4f4beab89df12e
Martijn van Beers <mail_dev@martijn.at>
2022-09-09 09:42:18 +0200

    Move description to external file

--- app.py
+++ app.py
@@ -280,22 +280,10 @@ lig = gradio.Interface(
     outputs="html",
 )
 
-iface = gradio.Parallel(hila, lig,
-                           title="Attention Rollout -- RoBERTa",
-                        description="""
-In this demo, we use the RoBERTa language model (optimized for masked language modelling and finetuned for sentiment analysis). 
-The model predicts for a given sentences whether it expresses a positive, negative or neutral sentiment.
-But how does it arrive at its classification?  This is, surprisingly perhaps, very difficult to determine.
-A range of so-called "attribution methods" have been developed that attempt to determine the importance of the words in the input for the final prediction; 
-they provide a very limited form of "explanation" -- and often disagree -- but sometimes provide good initial hypotheses nevertheless that can be further explored with other methods.
+with open("description.md", "r") as fh:
+    description = fh.read()
 
-Abnar & Zuidema (2020) proposed a method for Transformers called "Attention Rollout", which was further refined by Chefer et al. (2021) into Gradient-weighted Rollout. 
-Here we compare it to another popular method called Integrated Gradient.
-
-* Gradient-weighted attention rollout, as defined by [Hila Chefer](https://github.com/hila-chefer)
-  [(Transformer-MM_explainability)](https://github.com/hila-chefer/Transformer-MM-Explainability/), with rollout recursion upto selected layer
-* Layer IG, as implemented in [Captum](https://captum.ai/)(LayerIntegratedGradients), based on gradient w.r.t. selected layer.
-""",
+iface = gradio.Parallel(hila, lig, title="RoBERTa Explainability", description=description,
     examples=[
         [
             "This movie was the best movie I have ever seen! some scenes were ridiculous, but acting was great",
@@ -323,4 +311,5 @@ Here we compare it to another popular method called Integrated Gradient.
         ]
     ],
 )
+
 iface.launch()
---
+++ description.md
@@ -0,0 +1,12 @@
+In this demo, we use the RoBERTa language model (optimized for masked language modelling and finetuned for sentiment analysis).
+The model predicts for a given sentences whether it expresses a positive, negative or neutral sentiment.
+But how does it arrive at its classification?  This is, surprisingly perhaps, very difficult to determine.
+A range of so-called "attribution methods" have been developed that attempt to determine the importance of the words in the input for the final prediction;
+they provide a very limited form of "explanation" -- and often disagree -- but sometimes provide good initial hypotheses nevertheless that can be further explored with other methods.
+
+Abnar & Zuidema (2020) proposed a method for Transformers called "Attention Rollout", which was further refined by Chefer et al. (2021) into Gradient-weighted Rollout.
+Here we compare it to another popular method called Integrated Gradient.
+
+* Gradient-weighted attention rollout, as defined by [Hila Chefer](https://github.com/hila-chefer)
+  [(Transformer-MM_explainability)](https://github.com/hila-chefer/Transformer-MM-Explainability/), with rollout recursion upto selected layer
+* Layer IG, as implemented in [Captum](https://captum.ai/)(LayerIntegratedGradients), based on gradient w.r.t. selected layer.
