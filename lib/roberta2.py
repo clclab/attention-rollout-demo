66d59730357887599c3f03205bf44f12554c2dc5
Martijn van Beers <mail_dev@martijn.at>
2022-11-10 13:22:08 +0100

    Make the custom model work with IG
    IG doesn't create tensors that have `requires_grad`, so check for
    that before adding the hook

--- lib/roberta2.py
+++ lib/roberta2.py
@@ -280,7 +280,8 @@ class RobertaSelfAttention(nn.Module):
         attention_probs = nn.functional.softmax(attention_scores, dim=-1)
 
         self.save_attn(attention_probs)
-        attention_probs.register_hook(self.save_attn_gradients)
+        if attention_probs.requires_grad:
+            attention_probs.register_hook(self.save_attn_gradients)
 
         # This is actually dropping out entire tokens to attend to, which might
         # seem a bit unusual, but is taken from the original Transformer paper.
